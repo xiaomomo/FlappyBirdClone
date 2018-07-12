37a348800eb1f23f55466bd215e2b6d928c41850
liuqingjie <liuqingjie@weidian.com>
2018-07-12 23:58:50 +0800

    lession1

---
+++ lesson/lesson1.py
@@ -0,0 +1,33 @@
+import sys
+import pygame
+from pygame.locals import *
+
+screenWidth = 288
+screenHeight = 512
+
+pygame.init()
+pygame.display.set_caption('Flappy Bird')
+
+fpsClock = pygame.time.Clock()
+screen = pygame.display.set_mode((screenWidth, screenHeight))
+
+background = pygame.image.load('../assets/sprites/background-day.png').convert()
+player = pygame.image.load('../assets/sprites/redbird-upflap.png').convert_alpha()
+
+playerX = int(screenWidth * 0.2)
+playerY = int((screenHeight - player.get_height()) / 2)
+
+while True:
+    screen.blit(background, (0, 0))
+    screen.blit(player, (playerX, playerY))
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == KEYDOWN and event.key == K_SPACE:
+            print('tiaotiao')
+            playerY = playerY - 20
+
+    pygame.display.update()
+    fpsClock.tick(60)
